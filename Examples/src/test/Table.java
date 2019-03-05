c49d4e31eef31931c0e441c56cca268b84240338
Divya <gandikotadivya@gmail.com>
2019-03-05 17:00:02 -0500

    Code Optimisation
    Code Optimisation in print statement
    Added Comments

--- Examples/src/test/Table.java
+++ Examples/src/test/Table.java
@@ -1,20 +1,29 @@
 package test;
 
 public class Table {
-
+	// Table 3.
 	public void multiply(){
 		
 		for(int i=1;i<=10;i++) {
-			int j=i*3;
-			System.out.println(i+"*"+"3"+"="+j);
-			}
+		//	int j=i*3;
+			System.out.println(i+"*3="+(i*3));
+		}
 	}
 	public static void main(String[] args) {
 
 		Table t=new Table();
-		
 		t.multiply();
-		
 	}
-
 }
+/*output:
+	1*3=3
+	2*3=6
+	3*3=9
+	4*3=12
+	5*3=15
+	6*3=18
+	7*3=21
+	8*3=24
+	9*3=27
+	10*3=30
+*/
