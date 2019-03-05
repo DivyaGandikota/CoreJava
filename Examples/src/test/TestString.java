c1fc1a316f8e0076e26ec2c71f3bd4c9c47a1e31
Divya <gandikotadivya@gmail.com>
2019-03-05 17:00:59 -0500

    Access Modifiers
    Usage of Access Modifiers for variable declaration.
    Added Comments

--- Examples/src/test/TestString.java
+++ Examples/src/test/TestString.java
@@ -2,23 +2,30 @@ package test;
 
 public class TestString {
 	
-	String name="Divya";
-	String location="India";
-	String project="Java";
+	//Display values using class object
 	
-
+	private String name="Divya";
+	protected String location="India";
+	public String project="Java";
+	
+	// to access value in the private variable , we have to create a method.
+	/*public String getName() {
+		
+		return name;
+		
+	}*/
 	public static void main(String[] args) {
 		
 		TestString t=new TestString();
 		
-		System.out.println(t.name);
+		System.out.println(t.name); //t.getName()
 		System.out.println(t.location);
 		System.out.println(t.project);
 		
-		
 	}
-	
-	
-		
-	
 }
+/*output:
+	Divya
+	India
+	Java
+*/
