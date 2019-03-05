2b64b39560dbdc4ccc9be445db6c9772fb29af13
Divya <gandikotadivya@gmail.com>
2019-03-05 16:56:04 -0500

    Code Optimisation
    Code Optimisation and Added Comments with output

--- Examples/src/test/OddNumber.java
+++ Examples/src/test/OddNumber.java
@@ -1,12 +1,13 @@
 package test;
 
+//Print Odd Numbers only
 public class OddNumber {
 	
 	public void oddNum() {
 
 		for(int i=1;i<20;i++) {
-			int j=i%2;
-			if(j==1) {
+			//int j=i%2;
+			if(i%2==1) {
 				System.out.println(i);
 			}
 		}
@@ -20,3 +21,15 @@ public class OddNumber {
 	}
 
 }
+/*output:
+	1
+	3
+	5
+	7
+	9
+	11
+	13
+	15
+	17
+	19
+*/
