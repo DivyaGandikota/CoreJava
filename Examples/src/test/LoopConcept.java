af97b29f507247540ed60e7012241d35936af875
Divya <gandikotadivya@gmail.com>
2019-03-05 16:54:54 -0500

    Added Comments
    Added Comments

--- Examples/src/test/LoopConcept.java
+++ Examples/src/test/LoopConcept.java
@@ -1,5 +1,5 @@
 package test;
-
+//Print numbers from 1 to 10
 public class LoopConcept {
 	
 	public static void main(String[] args) {
@@ -27,3 +27,28 @@ public class LoopConcept {
 	}
 
 }
+
+/*Output:
+	for loop
+	1
+	2
+	3
+	4
+	5
+	6
+	7
+	8
+	9
+	10
+	while loop
+	1
+	2
+	3
+	4
+	5
+	6
+	7
+	8
+	9
+	10
+	*/
