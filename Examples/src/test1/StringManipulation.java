c83124e4a70269646a884bb4a0eb3d4f3ca3905d
Divya <gandikotadivya@gmail.com>
2019-03-08 15:28:37 -0500

    Revert "Operations on String"
    This reverts commit 06def7c551183e6052a5436887905b919c00a682.

--- Examples/src/test1/StringManipulation.java
+++ Examples/src/test1/StringManipulation.java
@@ -2,72 +2,52 @@ package test1;
 
 public class StringManipulation {
 	
-	String s;
-	
-	public StringManipulation() {
-		s = new String("I like Computing");
-	}
-	public void setString(String s) {
-		this.s=s;
-	}
+	String s = new String("I like Computing");
 	
 	public static void main(String[] args) {
+		
+		
 		
 		StringManipulation str=new StringManipulation();
 		
 		str.length();
-		str.uppercase();
-		str.lowercase();
-		str.subString();
-		str.indexAt();
-		str.charAtIndex();
-		str.isEmptyString();
-		str.trimString();	
 		
-		System.out.println();
-		str.setString("selenium");
-		str.length();
-		str.uppercase();
+		str.manipulation("Selenium");
+		
 	}
 	public void length() {
+		
 		System.out.println("Length: "+s.length());
+		
 	}
-	public void uppercase() {
+	
+	public void manipulation(String s) {
+		System.out.println("Length: "+s.length());
 		System.out.println("UpperCase is: "+ s.toUpperCase());
-	}
-	public void lowercase() {
 		System.out.println("LowerCase is: "+ s.toLowerCase());
-	}
-	public void charAtIndex() {
 		System.out.println("Char at: " +s.charAt(3));
-	}
-	public void subString() {
 		System.out.println("Sub String: "+s.substring(2, 7));
-	}
-	public void indexAt() {
 		System.out.println("Index at: "+s.indexOf("in"));
-	}
-	public void isEmptyString() {
 		System.out.println("Is Empty: "+ s.isEmpty());
-	}
-	public void trimString() {
 		System.out.println("Trim: " + s.trim());
 	}
-
 }
 
-/*
- * Output:
+/*Output:
 	Length: 16
 	UpperCase is: I LIKE COMPUTING
 	LowerCase is: i like computing
+	Char at: i
 	Sub String: like 
 	Index at: 13
-	Char at: i
 	Is Empty: false
 	Trim: I like Computing
-	
 	Length: 8
 	UpperCase is: SELENIUM
-
-*/
+	LowerCase is: selenium
+	Char at: e
+	Sub String: leniu
+	Index at: -1
+	Is Empty: false
+	Trim: Selenium
+	*/
