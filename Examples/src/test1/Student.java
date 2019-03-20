f6b2c4744fc05240bb57a34d13b63fa274bd9b1a
Divya <gandikotadivya@gmail.com>
2019-03-20 09:11:40 -0400

    Using BufferedReader
    Taking inputs from user, example.

--- Examples/src/test1/Student.java
+++ Examples/src/test1/Student.java
@@ -1,5 +1,10 @@
 package test1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Student {
 	
 	public String name;
@@ -19,12 +24,37 @@ public class Student {
 		this.age=a;
 		//System.out.println("Name: "+name + " , Age: "+ age);
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		Student student=new Student();
 		
+		/*Scanner userinput= new Scanner(System.in);
+		System.out.println("Input name and age: ");
+		String output= userinput.nextLine();
+		System.out.println(output);
+		userinput.close();*/
+	/*	
 		student.display();
 		student.edit("Amy",20);
-		student.display();
+		student.display();*/
+		
+		//using Buffered Reader for user Input
+		BufferedReader br = new BufferedReader(new   InputStreamReader(System.in));
+	       
+        System.out.println("Enter name");
+        String name=br.readLine();
+
+        System.out.println("Enter  age");
+        int age = Integer.parseInt(br.readLine());
+       
+        System.out.println("Enter marks");
+        double marks = Double.parseDouble(br.readLine());
+
+        System.out.println("Enter Gender(M/F)");
+        char gender=(char)br.read();
+        
+        System.out.println("Student Information:- " + "\nName is:"+name+"\n"+"Age is" + age+"\n"+"marks is:"+marks +"\nGender is:"+gender);
+
+				
 	}
 }
